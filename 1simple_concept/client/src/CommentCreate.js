750603d6826ec312679e136490d8353df6e5daa8
thekogo <thekogo@gmail.com>
2021-03-19 12:05:22 +0700

    first commit

---
+++ 1simple_concept/client/src/CommentCreate.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+
+export default function CommentCreate({ postId }) {
+  const [content, setContent] = useState('');
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    await axios.post(`http://posts.com/posts/${postId}/comments`, {
+      content
+    });
+
+    setContent('');
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>New Comment</label>
+          <input 
+            value={content} 
+            className="form-control"
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  )
+}
